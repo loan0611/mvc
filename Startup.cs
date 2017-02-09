e4e3880eee6e60e08fe02510ccd75f01c841f920
loannt <nguyenthimyloan061196@gmail.com>
2017-02-09 18:57:45 +0700

    first commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChiaSeTL.Startup))]
+namespace ChiaSeTL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
